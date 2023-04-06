d879c9886376cf01e1cf067ccfdddc4569ba870d
unknown <Gordon Hook@SC0-8.digital.lcl>
2023-04-06 17:59:03 +0300

    forst-commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ButtonCustom from './components/Button/Button'
+
+const App = () => {
+  return (
+    <div>
+        <h1>Hello</h1>
+        <ButtonCustom/>
+    </div>
+  )
+}
+
+export default App
